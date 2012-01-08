5114da288f7f1ce82efadd68d562236cf5116138
Karsten Loesing <karsten.loesing@gmx.net>
2012-01-08 21:11:39 +0100

    Start defining the bridge status interface.

--- src/org/torproject/descriptor/BridgeNetworkStatus.java
+++ src/org/torproject/descriptor/BridgeNetworkStatus.java
@@ -2,6 +2,14 @@
  * See LICENSE for licensing information */
 package org.torproject.descriptor;
 
+import java.util.SortedMap;
+
 public interface BridgeNetworkStatus extends Descriptor {
+
+  /* Return the published time in milliseconds. */
+  public long getPublishedMillis();
+
+  /* Return status entries, one for each contained bridge. */
+  public SortedMap<String, NetworkStatusEntry> getStatusEntries();
 }
 
